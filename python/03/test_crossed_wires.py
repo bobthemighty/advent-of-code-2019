ba625c1fa7821420c3df3a90a0fd5faca33c63ba
Bob Gregory <bob.gregory@cazoo.co.uk>
2019-12-07 12:12:31 +0000

    Acceptance tests pass

--- python/03/test_crossed_wires.py
+++ python/03/test_crossed_wires.py
@@ -22,7 +22,7 @@ class Point:
 
     @property
     def distance(self):
-        return self.x + self.y
+        return abs(self.x) + abs(self.y)
 
     def __lt__(self, other):
         return self.distance < other.distance
@@ -93,3 +93,21 @@ def test_intersections():
     b = Path(['U7','R6','D4','L4'])
 
     assert a.closest_intersection(b) == Point(3, 3)
+
+
+@pytest.mark.parametrize("a, b, expected",[
+    ('R75,D30,R83,U83,L12,D49,R71,U7,L72',
+     'U62,R66,U55,R34,D71,R55,D58,R83',
+     159
+    ),
+    ('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+     'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',
+     135
+    )
+])
+def test_examples(a, b, expected):
+   path_a = Path(a.split(','))
+   path_b = Path(b.split(','))
+
+   crossing = path_a.closest_intersection(path_b)
+   assert crossing.distance == expected
