f23d99022e0e1a2ea62d036641def0b803e699a0
Bob Gregory <bob.gregory@cazoo.co.uk>
2019-12-08 16:56:14 +0000

    Part 2 success

--- python/05/__main__.py
+++ python/05/__main__.py
@@ -4,7 +4,8 @@ from opcodes import Computer
 if __name__ == "__main__":
      [data] = fileinput.input()
      tape = [int(x) for x in data.split(',')]
-     prog = Computer(tape, [1])
-     prog.run()
+     prog = Computer(tape)
+     prog.run([1])
+     prog.run([5])
      print(prog.output)
 
