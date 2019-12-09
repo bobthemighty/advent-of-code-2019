6f3d42b08e0590d118ea1ef86ec52e3faf4a5f6b
Bob Gregory <bob.gregory@cazoo.co.uk>
2019-12-09 21:24:45 +0000

    Day 6 part 1

---
+++ python/06/test_orbits.py
@@ -0,0 +1,85 @@
+import fileinput
+
+class Universe:
+
+    def __init__(self):
+        self.objects = {}
+
+    def add(self, child, parent):
+        self[parent].append(self[child])
+
+    def __getitem__(self, name):
+        obj = self.objects.get(name)
+        if obj is None:
+            obj = Mass(name)
+            self.objects[name] = obj
+        return obj
+
+    @property
+    def total_orbits(self):
+        return sum(o.orbits for o in self.objects.values())
+
+
+class Mass:
+
+    def __init__(self, name):
+        self.name = name
+        self.parent = None
+
+    def append(self, child):
+        child.parent = self
+
+    @property
+    def orbits(self):
+        if self.parent is None:
+            return 0
+        return 1 + self.parent.orbits
+
+
+def test_centre():
+    obj = Mass('com')
+    assert obj.orbits == 0
+
+def test_child():
+    com = Mass('com')
+
+    a = Mass('a')
+    b = Mass('b')
+
+    a.append(b)
+    com.append(a)
+
+    assert a.orbits == 1
+    assert b.orbits == 2
+
+
+def test_total_orbits():
+
+    universe = Universe()
+    universe.add('b', 'a')
+    universe.add('a', 'com')
+
+    assert universe.total_orbits == 3
+
+def test_acceptance():
+    universe = Universe()
+    universe.add('b', 'com')
+    universe.add('c', 'b')
+    universe.add('d', 'c')
+    universe.add('e', 'd')
+    universe.add('f', 'e')
+    universe.add('g', 'b')
+    universe.add('h', 'g')
+    universe.add('i', 'd')
+    universe.add('j', 'e')
+    universe.add('k', 'j')
+    universe.add('l', 'k')
+
+    assert universe.total_orbits == 42
+
+if __name__ == "__main__":
+    universe = Universe()
+    for parent, child in (line.split(')') for line in fileinput.input()):
+        universe.add(child.strip(), parent.strip())
+
+    print(universe.total_orbits)
